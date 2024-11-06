439b710ad5a3db3bb6013f78eadbe8d96525bf0f
Stefaniya Ruseva <118391541+StefRuseva88@users.noreply.github.com>
2024-11-06 17:52:47 +0200

    Add files via upload

---
+++ RestSharpServices/Models/Label.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RestSharpServices.Models
+{
+    public class Label
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+    }
+}
